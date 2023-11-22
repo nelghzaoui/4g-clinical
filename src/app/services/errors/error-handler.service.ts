9fb7bce4c72805486156c4193b275d9f4987fc78
Nasreddine <eln@outlook.be>
2023-11-22 01:08:46 +0100

    Add error message notification using MatSnackBar

--- src/app/services/errors/error-handler.service.ts
+++ src/app/services/errors/error-handler.service.ts
@@ -1,8 +1,13 @@
 import { ErrorHandler, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class ErrorHandlerService implements ErrorHandler {
+  constructor(private readonly matSnackBar: MatSnackBar) {}
+
   handleError(error: any): void {
-    console.error(error);
+    this.matSnackBar.open(error.message, 'Close', {
+      duration: 5000,
+    });
   }
 }
